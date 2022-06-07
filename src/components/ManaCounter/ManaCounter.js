c182a0475631deb32591c9120ca533a51306baf3
tjbass2021 <tjbass2021@gmail.com>
2022-06-06 23:38:54 -0300

    feat: criação do contador de mana

---
+++ src/components/ManaCounter/ManaCounter.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+const ManaCounter = () => {
+    const [red, setRed] = useState(0);
+    const [blue, setBlue] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [black, setBlack] = useState(0);
+    const [white, setWhite] = useState(0);
+    const [nocolor, setNoColor] = useState(0);
+
+    const addMana = (color) => ++color;
+    const subMana = (color) => {
+        if(color <=0) {
+            return color = 0;
+        }else{
+            return --color;
+        }
+    }
+    return(
+        <div>
+            <h4>Mana Counter</h4>
+            <div className="botoes">
+                <button className="red" onClick={() => setRed(addMana(red))}>{red}</button>
+
+                <button className="red" onClick={() => setRed(subMana(red))}>-</button>
+            </div>
+            <div className="botoes">
+                <button className="blue" onClick={() => setBlue(addMana(blue))}>{blue}</button>
+
+                <button className="blue" onClick={() => setBlue(subMana(blue))}>-</button>
+            </div>
+            <div className="botoes">
+                <button className="green" onClick={() => setGreen(addMana(green))}>{green}</button>
+
+                <button className="green" onClick={() => setGreen(subMana(green))}>-</button>
+            </div>
+            <div className="botoes">
+                <button className="black" onClick={() => setBlack(addMana(black))}>{black}</button>
+
+                <button className="black" onClick={() => setBlack(subMana(black))}>-</button>
+            </div>
+            <div className="botoes">
+                <button className="white" onClick={() => setWhite(addMana(white))}>{white}</button>
+
+                <button className="white" onClick={() => setWhite(subMana(white))}>-</button>
+            </div>
+            <div className="botoes">
+                <button className="nocolor" onClick={() => setNoColor(addMana(nocolor))}>{nocolor}</button>
+
+                <button className="nocolor" onClick={() => setNoColor(subMana(nocolor))}>-</button>
+            </div>
+        </div>
+    );
+}
+
+export default ManaCounter;
